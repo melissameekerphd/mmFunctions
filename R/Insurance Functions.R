9759ccca128ef8919c8e559350478c574934258f
melissameekerphd <mmeeker@mgh.harvard.edu>
2026-02-10 11:49:57 -0500

    Adding function 'categorize_insurance()'.

---
+++ R/Insurance Functions.R
@@ -0,0 +1,27 @@
+#' Categorize Insurance
+#'
+#' `categorize_insurance` categorizes the unique combinations of a patient's
+#' payor and benefit plan as one of the following: Commercial, Medicare,
+#' Medicaid, MassHealth Limited, Auto, Workers' Comp, Other, or Missing.
+#'
+#' @param insurance_list the vector of patients' insurance. The values must be
+#' formatted as
+#' "Payor - insert_payor_name_here; Plan - insert_benefit_plan_name_here". For
+#' example, "Payor -  AETNA; Plan - AETNA PPO" or
+#' "Payor -  CIGNA; Plan - CIGNA LOCAL PLUS".
+#' @export
+
+categorize_insurance = function(insurance_list){
+
+  dictionary = insurance_codebook
+
+  dictionary$categorization = as.character(dictionary$categorization)
+
+  insurance_categorization = apply(insurance_list, 1, function(x) dictionary[which(dictionary$insurance==x),]$categorization)
+  insurance_categorization = factor(insurance_categorization, levels = c("1", "2", "3", "7", "4", "5", "6", "8"), labels = c("Commercial", "Medicare", "Medicaid", "MassHealth Limited", "Auto", "Workers' Comp", "Other", "Missing"))
+
+  return(insurance_categorization)
+
+}
+
+
